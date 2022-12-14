15ca9b82797a90c2acebbd2e97977214750526d0
NiyatovAziz <niyatovazizbek737.com>
2022-12-14 18:36:49 +0500

    Merchent-Added-Filters

---
+++ FurnitureShop/src/FurnitureShop.Merchant.Api/Filters/OrganizationFilterAttribute.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FurnitureShop.Merchant.Api.Filters;
+public class OrganizationFilterAttribute : TypeFilterAttribute
+{
+    public OrganizationFilterAttribute(string positions) : base(typeof(OrganizationAttribute))
+    {
+        Arguments = new object[] { positions };
+    }
+}
