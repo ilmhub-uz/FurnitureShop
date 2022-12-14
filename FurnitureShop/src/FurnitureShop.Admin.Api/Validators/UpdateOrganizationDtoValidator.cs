924ecbd71e3f5ce4defa1afc096598603ef36788
Jamoliddin <shermatovjamoliddin3@gmail.com>
2022-12-14 17:32:34 +0500

    added-updateorganizationdto-validator

---
+++ FurnitureShop/src/FurnitureShop.Admin.Api/Validators/UpdateOrganizationDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using FurnitureShop.Admin.Api.Dtos;
+
+namespace FurnitureShop.Admin.Api.Validators
+{
+    public class UpdateOrganizationDtoValidator : AbstractValidator<UpdateOrganizationDto>
+    {
+        public UpdateOrganizationDtoValidator()
+        {
+            RuleFor(updateorganizationdto => updateorganizationdto.Name).NotNull();
+            RuleFor(updateorganizationdto => updateorganizationdto.Name)
+           .MaximumLength(25).MinimumLength(3).When(createorganizationdto => createorganizationdto.Name is not null);
+        }
+    }
+}
