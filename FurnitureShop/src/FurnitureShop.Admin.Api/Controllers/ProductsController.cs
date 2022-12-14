4b3ad1088bbd8e457bcf7b4ad78636f3dbdbd5b6
Shokhzod Azamatov <ashotm1999@gmail.com>
2022-12-14 20:30:29 +0500

    add-controller

--- FurnitureShop/src/FurnitureShop.Admin.Api/Controllers/ProductsController.cs
+++ FurnitureShop/src/FurnitureShop.Admin.Api/Controllers/ProductsController.cs
@@ -6,4 +6,38 @@ namespace FurnitureShop.Admin.Api.Controllers;
 
 public class ProductsController : ControllerBase
 {
+    private readonly IProductsService _service;
+
+    public ProductsController(IProductsService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetProducts(ProductFilterDto product)
+    {
+        var products = await _service.GetProducts(product);
+        return Ok(products);
+    }
+
+    [HttpGet("{productId:Guid}")]
+    public async Task<IActionResult> GetProduct(Guid productId)
+    {
+        var product = await _service.GetProductByIdAsync(productId);
+        return Ok(product);
+    }
+
+    [HttpPut("{productId:Guid}")]
+    public async Task<IActionResult> UpdateProduct(Guid productId, UpdateProductDto dtoModel)
+    {
+        await _service.UpdateProduct(productId, dtoModel);
+        return Ok();
+    }
+
+    [HttpDelete("{productId:Guid}")]
+    public async Task<IActionResult> DeleteProduct(Guid productId)
+    {
+        await _service.DeleteProductById(productId);
+        return NoContent();
+    }
 }
