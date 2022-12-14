83f7db39545448804422b4ad31151e23ec8378df
Dostonbek Kurbanov <dostonbekkurbanov1@gmail.com>
2022-12-14 16:51:58 +0500

    devdoston/favorites-repository-interface-added

--- FurnitureShop/src/FurnitureShop.Data/Entities/FavoritesProduct.cs
+++ FurnitureShop/src/FurnitureShop.Data/Entities/FavoritesProduct.cs
@@ -1,17 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FurnitureShop.Data.Entities;
 
 public class FavoritesProduct
 {
     public Guid Id { get; set; }
-    public Guid ProductId { get; set; }
 
+    [Key]
+    public Guid UserId { get; set; }
+
+    [ForeignKey(nameof(UserId))]
+    public virtual AppUser? User { get; set; }
+
+    public Guid ProductId { get; set; }
     [ForeignKey(nameof(ProductId))]
     public virtual Product? Product { get; set; }
-
-    public Guid FavoritesId { get; set; }
-
-    [ForeignKey(nameof(FavoritesId))]
-    public virtual Favorites? Favorites { get; set; }
 }
