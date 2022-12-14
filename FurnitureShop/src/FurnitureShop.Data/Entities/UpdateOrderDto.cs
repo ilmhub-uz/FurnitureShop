4889319cb89536aae98b6466c2f3327032056e80
Akmalov-Asror <akmalovasror0@gmail.com>
2022-12-14 16:21:58 +0500

    UpdateOrderDto and IOrderInterface

---
+++ FurnitureShop/src/FurnitureShop.Data/Entities/UpdateOrderDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FurnitureShop.Data.Entities;
+
+public class UpdateOrderDto
+{
+    public Guid Id { get; set; }
+
+    public Guid OrganizationId { get; set; }
+    public virtual Organization? Organization { get; set; }
+
+    public Guid UserId { get; set; }
+    public virtual AppUser? User { get; set; }
+
+    public EOrderStatus Status { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime? LastUpdatedAt { get; set; }
+
+    public virtual ICollection<OrderProduct>? OrderProducts { get; set; }
+}
