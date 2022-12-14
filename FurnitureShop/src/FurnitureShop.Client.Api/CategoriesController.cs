c5853061845776b755b57108ce10b198130ccc4b
ext1z <ext1z1tabgod@gmail.com>
2022-12-14 15:27:27 +0500

    removed-unnecessary-items

--- FurnitureShop/src/FurnitureShop.Client.Api/CategoriesController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace FurnitureShop.Client.Api;
-[Route("api/[controller]")]
-[ApiController]
-public class CategoriesController : ControllerBase
-{
-}
