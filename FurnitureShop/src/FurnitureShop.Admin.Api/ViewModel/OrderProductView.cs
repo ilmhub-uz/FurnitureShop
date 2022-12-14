e24c63cd9e588423966809696cb656bab3c9f6b8
Akmalov-Asror <akmalovasror0@gmail.com>
2022-12-14 16:34:36 +0500

    new OrderView And OrderProductView

---
+++ FurnitureShop/src/FurnitureShop.Admin.Api/ViewModel/OrderProductView.cs
@@ -0,0 +1,14 @@
+﻿using FurnitureShop.Data.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FurnitureShop.Admin.Api.ViewModel;
+
+public class OrderProductView
+{
+
+    public Guid ProductId { get; set; }
+
+    public Guid OrderId { get; set; }
+    public uint Count { get; set; }
+    public string? Properties { get; set; }
+}
