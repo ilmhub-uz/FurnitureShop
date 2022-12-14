010391eb9fd10badfc2cb3b1031fc06440a2cd6f
Akmalov-Asror <akmalovasror0@gmail.com>
2022-12-14 18:50:32 +0500

    OrderView , OrderProductView

---
+++ FurnitureShop/src/FurnitureShop.Client.Api/ViewModel/OrderProductView.cs
@@ -0,0 +1,17 @@
+﻿using FurnitureShop.Data.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FurnitureShop.Client.Api.ViewModel
+{
+    public class OrderProductView
+    {
+        public Guid Id { get; set; }
+
+        public virtual ProductView? Product { get; set; }
+
+        public virtual OrderView? Order { get; set; }
+
+        public uint Count { get; set; }
+        public string? Properties { get; set; }
+    }
+}
