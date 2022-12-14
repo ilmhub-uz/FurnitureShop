349c8d9acf87f64517e4199d8c71bb8b9bbbef80
Jamoliddin <shermatovjamoliddin3@gmail.com>
2022-12-14 19:22:06 +0500

    validator

--- FurnitureShop/src/FurnitureShop.Admin.Api/Controllers/OrganizationsController.cs
+++ FurnitureShop/src/FurnitureShop.Admin.Api/Controllers/OrganizationsController.cs
@@ -2,10 +2,15 @@
 using FurnitureShop.Admin.Api.ViewModel;
 using FurnitureShop.Admin.Api.Services;
 using Microsoft.AspNetCore.Mvc;
+using FurnitureShop.Admin.Api.Filters;
+using FurnitureShop.Data.Entities;
+using FurnitureShop.Data.Repositories;
+
 namespace FurnitureShop.Admin.Api.Controllers;
 
 [Route("api/organizations")]
 [ApiController]
+[TypeFilter(typeof(IsCategoryExistsFilterAttribute))]
 public class OrganizationsController : ControllerBase
 {
     private readonly IOrganizationsService _service;
@@ -22,9 +27,10 @@ public class OrganizationsController : ControllerBase
         var organization = await _service.GetOrganizationsAsync();
         return Ok(organization);
     }
-
+    
     [HttpGet("{organizationId:Guid}", Name = "OrganizationById")]
     [ProducesResponseType(typeof(OrganizationView), StatusCodes.Status200OK)]
+    
     public async Task<IActionResult> GetOrganization(Guid organizationId)
     {
         var organization = await _service.GetOrganizationByIdAsync(organizationId);
