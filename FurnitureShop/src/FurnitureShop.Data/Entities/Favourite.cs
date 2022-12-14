fe1667544a2470854b1c16aea3f3d9a5345922e9
shakh <user@WIN-HFVP9OKK195>
2022-12-14 16:25:56 +0300

    added Favourite and FavouriteProduct Entities

---
+++ FurnitureShop/src/FurnitureShop.Data/Entities/Favourite.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureShop.Data.Entities
+{
+    public class Favourite
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public virtual AppUser? User { get; set; }
+        public virtual ICollection<FavouriteProduct>? FavouriteProducts { get; set; }
+    }
+}
---
+++ FurnitureShop/src/FurnitureShop.Data/Entities/FavouriteProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureShop.Data.Entities
+{
+    public class FavouriteProduct
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public virtual Product? Product { get; set; }
+        public Guid FavouriteId { get; set; }
+        public virtual Favourite? Favourite { get; set; }
+    }
+}
