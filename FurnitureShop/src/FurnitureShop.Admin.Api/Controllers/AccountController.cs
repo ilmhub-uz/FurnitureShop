f0cf04b0198e453e727735467fd3ac24a62ba55e
Jamoliddin <shermatovjamoliddin3@gmail.com>
2022-12-14 21:16:47 +0500

    added-signup

--- FurnitureShop/src/FurnitureShop.Admin.Api/Controllers/AccountController.cs
+++ FurnitureShop/src/FurnitureShop.Admin.Api/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using FurnitureShop.Data.Entities;
+﻿using FurnitureShop.Admin.Api.Dtos;
+using FurnitureShop.Data.Entities;
+using Mapster;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -12,12 +14,25 @@ public class AccountController : ControllerBase
 {
 
     private readonly SignInManager<AppUser> _signInManager;
-
-    public AccountController(SignInManager<AppUser> signInManager)
+    private readonly UserManager<AppUser> _userManager;
+    public AccountController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
     {
         _signInManager = signInManager;
+        _userManager = userManager;
     }
+    [HttpPost("signup")]
+    public async Task<IActionResult> SignUp([FromForm] RegisterUserDto registerUserDto)
+    {
 
+        var user = registerUserDto.Adapt<AppUser>();
+        var result = await _userManager.CreateAsync(user, registerUserDto.Password);
+
+        if (!result.Succeeded)
+            return BadRequest();
+
+        await _signInManager.SignInAsync(user, true);
+        return Ok();
+    }
     [HttpPost("signin")]
     public async Task<IActionResult> SignIn(string userName, string password)
     {
---
+++ FurnitureShop/src/FurnitureShop.Admin.Api/Dtos/RegisterUserDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FurnitureShop.Admin.Api.Dtos
+{
+    public class RegisterUserDto
+    {
+        [Required]
+        public string? UserName { get; set; }
+
+        [Required]
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        [Required]
+        public string? Email { get; set; }
+    }
+}
