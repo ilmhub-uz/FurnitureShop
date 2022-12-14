523d8052ecc0f21acc21c678bed8550939685373
A3S4ROBOY <110540334+Akmalov-Asror@users.noreply.github.com>
2022-12-15 01:23:15 +0500

    new filter

---
+++ FurnitureShop/src/FurnitureShop.Client.Api/Filters/UpdateOrderFilterAttribute.cs
@@ -0,0 +1,26 @@
+﻿using FurnitureShop.Data.Repositories;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace FurnitureShop.Client.Api.Filters;
+
+public class UpdateOrderFilterAttribute : ActionFilterAttribute
+{
+
+    private readonly UnitOfWork _unitOfWork;
+
+    public UpdateOrderFilterAttribute(UnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        if (!context.ActionArguments.ContainsKey("updateOrderDto") && context.ActionArguments.ContainsKey("orderId"))
+        {
+            await next();
+            return;
+        }
+
+        await next();
+    }
+}
