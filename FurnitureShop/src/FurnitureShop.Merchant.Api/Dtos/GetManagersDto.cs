14706ca662dea718bf857f9ac3bc5d51d3752be8
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2022-12-15 03:35:46 +0500

    Added GetManagersDto

---
+++ FurnitureShop/src/FurnitureShop.Merchant.Api/Dtos/GetManagersDto.cs
@@ -0,0 +1,11 @@
+﻿using FurnitureShop.Data.Entities;
+
+namespace FurnitureShop.Merchant.Api.Dtos
+{
+    public class GetManagersDto
+    {
+        public Guid UserId { get; set; }
+        public AppUser User { get; set; }
+        public ERole Role { get; set; }
+    }
+}
