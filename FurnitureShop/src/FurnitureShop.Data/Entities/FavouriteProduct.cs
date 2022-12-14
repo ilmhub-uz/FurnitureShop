320c5801e47533c2351ae3b1951f57b3aca86e8a
shakh <user@WIN-HFVP9OKK195>
2022-12-14 17:03:50 +0300

    add FavouriteProduct

---
+++ FurnitureShop/src/FurnitureShop.Data/Entities/FavouriteProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureShop.Data.Entities
+{
+    public class FavoritesProduct
+    {
+        public Guid Id { get; set; }
+
+        [Key]
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public virtual AppUser? User { get; set; }
+        public Guid ProductId { get; set; }
+        [ForeignKey(nameof(ProductId))]
+        public virtual Product? Product { get; set; }
+    }
+}
