878bcdc53eaa969ba9da82439d559d579afb0b75
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2022-12-15 04:57:32 +0500

    Added IManagerService

---
+++ FurnitureShop/src/FurnitureShop.Merchant.Api/Services/IManagerService.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace FurnitureShop.Merchant.Api.Services
+{
+    public interface IManagerService
+    {
+        Task AddManager(ClaimsPrincipal User, Guid organizationId, string email);
+        Task<List<GetManagersDto>> GetManagers(Guid organizationId);
+        Task RemoveManager(Guid organizationId, Guid managerId);
+    }
+}
