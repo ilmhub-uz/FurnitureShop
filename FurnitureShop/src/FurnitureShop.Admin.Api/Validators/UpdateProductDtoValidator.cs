7e4639bc961394754e4dd7782a5013bcab732785
Leverow <info.enjoymedia@mail.ru>
2022-12-14 11:51:36 +0500

    Update Product Dto Validator added

---
+++ FurnitureShop/src/FurnitureShop.Admin.Api/Validators/UpdateProductDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using FurnitureShop.Admin.Api.Dtos;
+
+namespace FurnitureShop.Admin.Api.Validators;
+
+public class UpdateProductDtoValidator : AbstractValidator<UpdateProductDto>
+{
+    private readonly uint _minCountOfProduct = 0;
+    public UpdateProductDtoValidator()
+    {
+        RuleFor(dto => dto.Name).NotNull();
+        RuleFor(dto => dto.Price).NotNull();
+
+        RuleFor(dto => dto.Count).NotNull();
+        RuleFor(dto => dto.Count).GreaterThan(_minCountOfProduct);
+
+        RuleFor(dto => dto.CategoryId).NotNull();
+        RuleFor(dto => dto.CategoryId).GreaterThan(0);
+        
+        RuleFor(dto => dto.OrganizationId).NotEmpty();
+    }
+}
