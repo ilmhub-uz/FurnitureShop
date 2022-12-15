ef8b439dba8d0772710b913c09c2e08f520a1c5e
shakh <user@WIN-HFVP9OKK195>
2022-12-15 19:11:40 +0300

    shakh/add Like - Entity

---
+++ FurnitureShop/src/FurnitureShop.Data/Entities/Like.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureShop.Data.Entities
+{
+    public class Like
+    {
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public int? Count { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public virtual AppUser? User { get; set; }
+        public Guid ProductId { get; set; }
+
+        [ForeignKey(nameof(ProductId))]
+        public virtual Product? Product { get; set; }
+    }
+}
