a2c313523a26540bfadada65524379d4e06d89a9
Nuriddin321 <donkoko1996@gmail.com>
2022-12-15 06:26:31 +0500

    Deleted unnecessary ProoductFilterDto

--- FurnitureShop/src/FurnitureShop.Admin.Api/Dtos/ProductFilterDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FurnitureShop.Admin.Api.Dtos;
-
-public class ProductFilterDto
-{
-    public uint Page { get; set; }
-    public uint Limit { get; set; }
-}
--- FurnitureShop/src/FurnitureShop.Admin.Api/Services/IProductsService.cs
+++ FurnitureShop/src/FurnitureShop.Admin.Api/Services/IProductsService.cs
@@ -5,7 +5,7 @@ namespace FurnitureShop.Admin.Api.Services;
 
 public interface IProductsService
 {
-    Task<List<ProductView>> GetProducts(ProductFilterDto filter);
+    Task<List<ProductView>> GetProducts();
     Task<ProductView> GetProductByIdAsync(Guid productId);
     Task UpdateProduct(Guid productId, UpdateProductDto dtoModel);
     Task DeleteProductById(Guid productId);
