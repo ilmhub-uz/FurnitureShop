d404b72e98a0e1dc03cdbb69ee64b1d36d57b7fa
A3S4ROBOY <110540334+Akmalov-Asror@users.noreply.github.com>
2022-12-15 18:34:25 +0500

    new validator

---
+++ FurnitureShop/src/FurnitureShop.Client.Api/Validators/CreateCartDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using FurnitureShop.Client.Api.Dtos;
+
+namespace FurnitureShop.Client.Api.Validators;
+
+public class CreateCartDtoValidator : AbstractValidator<CreateCartDto>
+{
+    public CreateCartDtoValidator()
+    {
+        RuleFor(createOrderDto => createOrderDto.ProductId).NotEmpty();
+        RuleFor(createOrderDto => createOrderDto.Count).NotEmpty();
+        RuleFor(createOrderDto => createOrderDto.Properties).NotEmpty();
+    }
+}
