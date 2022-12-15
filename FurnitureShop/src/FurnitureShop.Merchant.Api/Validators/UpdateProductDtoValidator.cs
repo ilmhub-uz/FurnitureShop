42cab1c8ed40a73c484908b95b620d684fd239ae
abuUsman_04 <i123l183l@gmail.com>
2022-12-15 13:38:58 +0300

    added validators, removed unnecessary things

---
+++ FurnitureShop/src/FurnitureShop.Merchant.Api/Validators/UpdateProductDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using FurnitureShop.Merchant.Api.Dtos;
+
+namespace FurnitureShop.Merchant.Api.Validators;
+public class UpdateProductDtoValidator : AbstractValidator<UpdateProductDto>
+{
+    public UpdateProductDtoValidator()
+    {
+        RuleFor(productDto => productDto.Name).Length(1, 50)
+            .When(product => product.Name != null);
+
+        RuleFor(productDto => productDto.Price).NotEmpty();
+
+        RuleFor(productDto => productDto.CategoryId).NotEmpty();
+
+        RuleFor(productDto => productDto.OrganizationId).NotEmpty();
+    }
+}
