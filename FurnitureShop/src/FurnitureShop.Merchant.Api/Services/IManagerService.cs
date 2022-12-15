5b313dde74f63be84e453c97bf189fa1fc22147b
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2022-12-15 07:30:48 +0500

    Fix

--- FurnitureShop/src/FurnitureShop.Merchant.Api/Services/IManagerService.cs
+++ FurnitureShop/src/FurnitureShop.Merchant.Api/Services/IManagerService.cs
@@ -1,11 +1,12 @@
-﻿using System.Security.Claims;
+﻿using FurnitureShop.Merchant.Api.ViewModel;
+using System.Security.Claims;
 
 namespace FurnitureShop.Merchant.Api.Services
 {
     public interface IManagerService
     {
         Task AddManager(ClaimsPrincipal User, Guid organizationId, string email);
-        Task<List<GetManagersDto>> GetManagers(Guid organizationId);
+        Task<List<GetManagersView>> GetManagers(Guid organizationId);
         Task RemoveManager(Guid organizationId, Guid managerId);
     }
 }
