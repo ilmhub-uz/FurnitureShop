088dc5016ecc7bb9fdf54b0d00e49641788544f1
Jamoliddin <shermatovjamoliddin3@gmail.com>
2022-12-15 09:41:29 +0500

    added order pagianation

--- FurnitureShop/src/FurnitureShop.Admin.Api/Services/IOrdersService.cs
+++ FurnitureShop/src/FurnitureShop.Admin.Api/Services/IOrdersService.cs
@@ -1,11 +1,12 @@
 ﻿using FurnitureShop.Admin.Api.Dtos;
 using FurnitureShop.Admin.Api.ViewModel;
+using FurnitureShop.Common.Models;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace FurnitureShop.Admin.Api.Services;
 
 public interface IOrdersService
 {
-    Task<List<OrderView>> GetOrdersAsync(OrderFilter filter);
+    Task<List<OrderView>> GetOrdersAsync(OrderFilter filter, PaginationParams paginationParams);
     Task<OrderView> GetOrderByIdAsync(Guid orderId);
 }
