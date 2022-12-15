ef586c22a7c237375b2fad06dd1576f7b13139c7
munissumii <monikatillayeva2021@gmail.com>
2022-12-15 19:57:54 +0500

    Update CreateProductCommentValidator.cs

--- FurnitureShop/src/FurnitureShop.Client.Api/Validators/CreateProductCommentValidator.cs
+++ FurnitureShop/src/FurnitureShop.Client.Api/Validators/CreateProductCommentValidator.cs
@@ -1,6 +1,14 @@
-﻿namespace FurnitureShop.Client.Api.Validators
+﻿using FluentValidation;
+using FurnitureShop.Data.Entities;
+
+namespace FurnitureShop.Client.Api.Validators;
+
+public class CreateProductCommentValidator : AbstractValidator<ProductComment>
 {
-    public class CreateProductCommentValidator
+    public CreateProductCommentValidator()
     {
+        RuleFor(c => c.Comment).MinimumLength(1);
+        RuleFor(c => c.Comment).MaximumLength(200);
+        RuleFor(c => c.Comment).NotEmpty();
     }
 }
