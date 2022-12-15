3941cea2354fd3aac5a3fe7bba0206b756df4cda
shakh <user@WIN-HFVP9OKK195>
2022-12-15 20:10:08 +0300

    Shakh/LikeEntity + DbSet

---
+++ FurnitureShop/src/FurnitureShop.Data/Entities/Like.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureShop.Data.Entities
+{
+    public class LikeProduct
+    {
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public virtual AppUser? User { get; set; }
+
+        public Guid ProductId { get; set; }
+        [ForeignKey(nameof(ProductId))]
+        public virtual Product? Product { get; set; }
+    }
+}
