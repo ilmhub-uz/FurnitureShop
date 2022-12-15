f794c413ec79541f251da1745f6e12de43fc6996
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2022-12-15 08:07:01 +0500

    Added IEmailService, EmailService, EmailConfigurationDto

---
+++ FurnitureShop/src/FurnitureShop.Common/Email-Sender/Dtos/EmailConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureShop.Common.Email_Sender.Dtos;
+
+public class EmailConfiguration
+{
+    public string From { get; set; }
+    public string SmtpServer { get; set; }
+    public int Port { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
