a1b958658dbd1a358d1186f1702cc0dbb3630b15
A3S4ROBOY <110540334+Akmalov-Asror@users.noreply.github.com>
2022-12-16 18:14:17 +0500

    favouriteActionFilterAttribute

---
+++ FurnitureShop/src/FurnitureShop.Client.Api/Filters/IsFavouriteIdExistsFilterAttribute.cs
@@ -0,0 +1,34 @@
+﻿using FurnitureShop.Data.Context;
+using FurnitureShop.Data.Repositories;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FurnitureShop.Client.Api.Filters;
+
+public class IsFavouriteIdExistsFilterAttribute : ActionFilterAttribute
+{
+    private readonly AppDbContext _context;
+    public IsFavouriteIdExistsFilterAttribute(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        if (!context.ActionArguments.ContainsKey("favouriteId"))
+        {
+            context.Result = new NotFoundResult();
+            return;
+        }
+        var favouriteId = (int?)context.ActionArguments["favouriteId"];
+
+
+        if (!_context.FavouriteProducts.FirstOrDefault(favourite => favourite.ProductId == favouriteId))
+        {
+            context.Result = new NotFoundResult();
+            return;
+        }
+
+        await next();
+    }
+}
