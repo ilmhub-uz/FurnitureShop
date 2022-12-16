16ac699d6937531a8e553d5607a46cbe707a3f9b
umidbekozodov <ozodovumidbek8044@gmail.com>
2022-12-16 17:57:40 +0500

    IdValidationAttribute Added to Client OrganizationService

--- FurnitureShop/src/FurnitureShop.Client.Api/Services/OrganizationService.cs
+++ FurnitureShop/src/FurnitureShop.Client.Api/Services/OrganizationService.cs
@@ -1,6 +1,7 @@
 ﻿using FurnitureShop.Client.Api.Services.Interfaces;
 using FurnitureShop.Client.Api.ViewModel;
 using FurnitureShop.Common.Exceptions;
+using FurnitureShop.Common.Filters;
 using FurnitureShop.Data.Entities;
 using FurnitureShop.Data.Repositories;
 using JFA.DependencyInjection;
@@ -24,12 +25,11 @@ public class OrganizationService : IOrganizationService
         return (await _unitOfWork.Organizations.GetAll().ToListAsync()).Adapt<List<OrganizationView>>();
     }
 
+    [IdValidation]
     public async Task<OrganizationView> GetOrganizationByIdAsync(Guid organizationId)
     {
         var organization = await _unitOfWork.Organizations.GetAll().FirstOrDefaultAsync(org => org.Id == organizationId);
-        if (organization is null)
-            throw new NotFoundException<Organization>();
 
-        return organization.Adapt<OrganizationView>();
+        return organization!.Adapt<OrganizationView>();
     }
 }
