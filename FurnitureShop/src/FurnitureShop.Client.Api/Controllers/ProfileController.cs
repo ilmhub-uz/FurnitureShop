600cd1369a9565a3fcd1715048b4c5622477165e
ext1z <ext1z1tabgod@gmail.com>
2022-12-19 11:00:23 +0500

    added user profile

---
+++ FurnitureShop/src/FurnitureShop.Client.Api/Controllers/ProfileController.cs
@@ -0,0 +1,32 @@
+﻿using FurnitureShop.Client.Api.ViewModel;
+using FurnitureShop.Data.Entities;
+using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FurnitureShop.Client.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class ProfileController : ControllerBase
+{
+    private readonly UserManager<AppUser> _userManager;
+
+    public ProfileController(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+
+
+    [HttpGet("profile")]
+    [ProducesResponseType(typeof(UserView), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetUserProfile()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        return Ok(user.Adapt<UserView>());
+    }
+}
