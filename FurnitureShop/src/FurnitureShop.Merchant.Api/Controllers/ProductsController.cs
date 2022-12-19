0890ef832fb082f1c2f16666a351aa84cd6ffd38
umidbekozodov <ozodovumidbek8044@gmail.com>
2022-12-20 04:06:29 +0500

    RateProduct actioni savechange qilindi

--- FurnitureShop/src/FurnitureShop.Merchant.Api/Controllers/ProductsController.cs
+++ FurnitureShop/src/FurnitureShop.Merchant.Api/Controllers/ProductsController.cs
@@ -79,6 +79,7 @@ public partial class ProductsController : ControllerBase
             product.Rates = new List<uint>();
         
         product.Rates.Add(rate);
+        _context.SaveChangesAsync();
         return Ok();
     }
 
