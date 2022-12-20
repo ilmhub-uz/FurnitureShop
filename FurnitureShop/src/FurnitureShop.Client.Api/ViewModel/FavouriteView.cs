a25220e3e7c977c64b200dffe91ff814802293f3
A3S4ROBOY <110540334+Akmalov-Asror@users.noreply.github.com>
2022-12-20 17:54:45 +0500

    news

--- FurnitureShop/src/FurnitureShop.Client.Api/ViewModel/FavouriteView.cs
+++ FurnitureShop/src/FurnitureShop.Client.Api/ViewModel/FavouriteView.cs
@@ -1,17 +1,8 @@
-﻿using FurnitureShop.Data.Entities;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿namespace FurnitureShop.Client.Api.ViewModel;
 
-namespace FurnitureShop.Client.Api.ViewModel
+public class FavouriteView
 {
-        public class FavouriteView
-    {
-            public Guid Id { get; set; }
-            public Guid UserId { get; set; }
-
-            [ForeignKey(nameof(UserId))]
-            public virtual AppUser? User { get; set; }
-            public Guid ProductId { get; set; }
-            [ForeignKey(nameof(ProductId))]
-            public virtual Product? Product { get; set; }
-        }
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public Guid ProductId { get; set; }
 }
