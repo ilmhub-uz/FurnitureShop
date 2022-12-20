ee1849ef5bf699193e6031bddca03ffdc42638ea
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2022-12-20 22:26:26 +0500

    Updated Dto and EmployeeService

--- FurnitureShop/src/FurnitureShop.Merchant.Api/Dtos/EmployeeServiceDto.cs
+++ FurnitureShop/src/FurnitureShop.Merchant.Api/Dtos/EmployeeServiceDto.cs
@@ -4,7 +4,6 @@ namespace FurnitureShop.Merchant.Api.Dtos;
 
 public class EmployeeServiceDto
 {
-    public ClaimsPrincipal User { get; set; }
     public Guid OrganizationId { get; set; }
     public string Email { get; set; }
     public AddEmployeeRole ERole { get; set; }
