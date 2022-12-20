363ac4b80af383d01527d1e71e4ae83e7fd675c9
NiyatovAziz <niyatovazizbek737.com>
2022-12-20 14:36:21 +0500

    Merchant-Product-Pagination-Added

---
+++ FurnitureShop/src/FurnitureShop.Merchant.Api/Dtos/PriceAmounts.cs
@@ -0,0 +1,7 @@
+﻿namespace FurnitureShop.Merchant.Api.Dtos;
+
+public class PriceAmounts
+{
+    public uint MinAmount { get; set; } = uint.MinValue;
+    public uint MaxAmount { get; set; } = uint.MaxValue;
+}
---
+++ FurnitureShop/src/FurnitureShop.Merchant.Api/Helpers/ProductPagedListHelpers.cs
@@ -0,0 +1,70 @@
+﻿using FurnitureShop.Common.Helpers;
+using FurnitureShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FurnitureShop.Merchant.Api.Helpers;
+
+public static class ProductPagedListHelpers
+{
+   public static async Task<IEnumerable<T>> ToProductPagedListAsync<T>(this IQueryable<T> source, ProductPaganitionParams pageParams) where T : Product
+   {
+        if (pageParams.ProductName is not null)
+            source = source.Where(x => x.Name!.Contains(pageParams.ProductName));
+
+        if (pageParams.PriceAmounts is not null)
+        {
+            source = source.Where(x => x.Price >= pageParams.PriceAmounts.MinAmount && x.Price <= pageParams.PriceAmounts.MaxAmount);
+            source = source.OrderByDescending(x => x.Price);
+        }
+
+        if (pageParams.Views && pageParams.Price)
+            source = source.OrderByDescending(x => x.Price).ThenByDescending(x => x.Views);
+        else
+        {
+            if (pageParams.Views)
+                source = source.OrderByDescending(x => x.Views);
+
+            if (pageParams.Price)
+                source = source.OrderByDescending(x => x.Price);
+        }
+
+        if(pageParams.Rates)
+        {
+            source = source.Where(x => x.Rates != null);
+            source = source.OrderByDescending(x => x.Rates!.Select(x => (int)x).ToList().Sum() / x.Rates!.Count());
+        }
+
+        if (pageParams.ReverseOrder)
+            source = source.Reverse();
+
+        return await source.ToPagedListAsync(pageParams);
+   }
+
+    public static IEnumerable<T> ToProductPageList<T>(this IEnumerable<T> source, ProductPaganitionParams pageParams) where T : Product
+    {
+        if (pageParams.ProductName is not null)
+            source = source.Where(x => x.Name!.Contains(pageParams.ProductName));
+
+        if (pageParams.PriceAmounts is not null)
+        {
+            source = source.Where(x => x.Price >= pageParams.PriceAmounts.MinAmount && x.Price <= pageParams.PriceAmounts.MaxAmount);
+            source = source.OrderByDescending(x => x.Price);
+        }
+
+        if (pageParams.Views && pageParams.Price)
+            source = source.OrderByDescending(x => x.Price).ThenByDescending(x => x.Views);
+        else
+        {
+            if (pageParams.Views)
+                source = source.OrderByDescending(x => x.Views);
+
+            if (pageParams.Price)
+                source = source.OrderByDescending(x => x.Price);
+        }
+
+        if (pageParams.ReverseOrder)
+            source = source.Reverse();
+
+        return  source.ToPagedList(pageParams);
+    }
+}
