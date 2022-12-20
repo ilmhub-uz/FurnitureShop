1962e8d1665332462687567a72ff65c9a56837e3
ext1z <ext1z1tabgod@gmail.com>
2022-12-20 15:33:25 +0500

    repository and dbset

---
+++ FurnitureShop/src/FurnitureShop.Data/Migrations/20221220081318_CartAdded.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FurnitureShop.Data.Migrations
+{
+    public partial class CartAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "carts",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_carts", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_carts_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "cart_products",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    product_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    cart_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    count = table.Column<long>(type: "bigint", nullable: false),
+                    properties = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_cart_products", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_cart_products_carts_cart_id",
+                        column: x => x.cart_id,
+                        principalTable: "carts",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_cart_products_products_product_id",
+                        column: x => x.product_id,
+                        principalTable: "products",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_cart_products_cart_id",
+                table: "cart_products",
+                column: "cart_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_cart_products_product_id",
+                table: "cart_products",
+                column: "product_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_carts_user_id",
+                table: "carts",
+                column: "user_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cart_products");
+
+            migrationBuilder.DropTable(
+                name: "carts");
+        }
+    }
+}
