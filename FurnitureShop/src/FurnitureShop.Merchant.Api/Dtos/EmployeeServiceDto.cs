ae735258df4b5cc93cea34656471ef020c5c2e85
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2022-12-21 05:48:47 +0500

    Added EmailConfiguration to appsettings and updated EmployeeServiceDto

--- FurnitureShop/src/FurnitureShop.Merchant.Api/Dtos/EmployeeServiceDto.cs
+++ FurnitureShop/src/FurnitureShop.Merchant.Api/Dtos/EmployeeServiceDto.cs
@@ -1,9 +1,14 @@
-﻿using System.Security.Claims;
+﻿using FurnitureShop.Data.Entities;
+using System.Security.Claims;
 
 namespace FurnitureShop.Merchant.Api.Dtos;
 
 public class EmployeeServiceDto
 {
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Username { get; set; }
+    public EUserStatus Status { get; set; }
     public Guid OrganizationId { get; set; }
     public string Email { get; set; }
     public AddEmployeeRole ERole { get; set; }
