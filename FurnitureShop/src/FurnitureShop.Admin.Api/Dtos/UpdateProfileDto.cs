22d61d529e24b332c9b08fc6953435ef35fec7e2
Jamoliddin <shermatovjamoliddin3@gmail.com>
2022-12-21 18:05:46 +0500

    added-profile

---
+++ FurnitureShop/src/FurnitureShop.Admin.Api/Dtos/UpdateProfileDto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace FurnitureShop.Admin.Api.Dtos
+{
+    public class UpdateProfileDto
+    {
+        [Required]
+        public string? UserName { get; set; }
+
+        [Required]
+        public string? FirstName { get; set; }
+
+        [Required]
+        public string? Email { get; set; }
+    }
+}
