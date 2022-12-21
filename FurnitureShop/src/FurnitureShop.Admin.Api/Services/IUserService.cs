b6a3a94f0b7a715855238952a77b8b21b53bd7d4
Botirjon29 <110552031+Botirjon29@users.noreply.github.com>
2022-12-21 17:05:09 +0500

    added IUserService

---
+++ FurnitureShop/src/FurnitureShop.Admin.Api/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using FurnitureShop.Admin.Api.Dtos;
+using FurnitureShop.Data.Entities;
+
+namespace FurnitureShop.Admin.Api.Services;
+
+public interface IUserService
+{
+    Task<List<AppUser>> GetUsers(UserFilterDto userFilterDto);
+    Task<AppUser> GetUserById(Guid userId);
+    Task UpdateUser(Guid userId, UpdateUserDto updateUserDto);
+    Task DeleteUser(Guid userId);
+}
