b3a8691d516d7535fad93c52ffe59e1896adee4e
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2022-12-22 20:59:19 +0500

    Some changes

--- FurnitureShop/src/FurnitureShop.Api/ViewModel/OrganizationView.cs
+++ FurnitureShop/src/FurnitureShop.Api/ViewModel/OrganizationView.cs
@@ -6,5 +6,6 @@ public class OrganizationView
 {
     public Guid Id { get; set; }
     public string? Name { get; set; }
+    public string? Image { get; set; }
     public EOrganizationStatus Status { get; set; }
 }
