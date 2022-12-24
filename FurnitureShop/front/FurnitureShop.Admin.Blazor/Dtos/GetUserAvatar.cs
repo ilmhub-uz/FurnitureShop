9214bdc278c04b4bf6349b920e3246adc7ecc899
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2022-12-25 02:12:43 +0500

    Added feature: "If unauthorized user enter any admin page, he/she gets redirected to signIn page"

---
+++ FurnitureShop/front/FurnitureShop.Admin.Blazor/Dtos/GetUserAvatar.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FurnitureShop.Admin.Blazor.Dtos
+{
+    public class GetUserAvatar
+    {
+        [Required] 
+        public Guid UserId { get; set; }
+    }
+}
