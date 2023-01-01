199a97ffc0a89b2e936a95e1ccdec61860846c0e
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2023-01-01 23:18:17 +0500

    Added IRequestHelper interface

---
+++ FurnitureShop/front/Common/IRequestHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+
+namespace Common;
+
+public interface IRequestHelper
+{
+    public Task<HttpStatusCode> SendRequest<TModel>(string baseUri, string requestUri, TModel model);
+
+}
