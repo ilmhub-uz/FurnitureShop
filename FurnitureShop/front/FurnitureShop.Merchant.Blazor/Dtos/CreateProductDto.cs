73ebc4806895d1b43de5dbc6fc12cf0d2ef3f755
abuUsman_04 <i123l183l@gmail.com>
2023-01-03 21:23:41 +0300

    add product form added

--- FurnitureShop/front/FurnitureShop.Merchant.Blazor/Dtos/CreateProductDto.cs
+++ FurnitureShop/front/FurnitureShop.Merchant.Blazor/Dtos/CreateProductDto.cs
@@ -9,14 +9,14 @@ public class CreateProductDto
     public string? Description { get; set; }
     public Dictionary<string, string>? Properties { get; set; }
     [Required]
-    public decimal Price { get; set; }
+    public decimal? Price { get; set; }
     public bool OnTrend { get; set; }
     public bool OnSale { get; set; }
     [Required]
-    public uint Count { get; set; }
+    public uint? Count { get; set; }
 
     [Required]
-    public int CategoryId { get; set; }
+    public int? CategoryId { get; set; }
     [Required]
-    public Guid OrganizationId { get; set; }
+    public Guid? OrganizationId { get; set; }
 }
