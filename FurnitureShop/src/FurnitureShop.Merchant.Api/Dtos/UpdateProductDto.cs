e2099141c4afbc8665c8c434a6efe5f26fdc2505
NiyatovAziz <niyatovazizbek737.com>
2023-01-05 16:50:11 +0500

    Merchant-Fixed-UpdateProduct

--- FurnitureShop/src/FurnitureShop.Merchant.Api/Dtos/UpdateProductDto.cs
+++ FurnitureShop/src/FurnitureShop.Merchant.Api/Dtos/UpdateProductDto.cs
@@ -6,11 +6,13 @@ public class UpdateProductDto
 {
     public string? Name { get; set; }
     public string? Description { get; set; }
+    public bool? WithInstallation { get; set; }
+    public string? Brend { get; set; }
+    public string? Material { get; set; }
     public Dictionary<string, string>? Properties { get; set; }
     public decimal Price { get; set; }
-    public bool OnTrend { get; set; }
-    public bool OnSale { get; set; }
-    public uint? Count { get; set; }
+    public bool IsAvailable { get; set; }
+    public uint Count { get; set; }
     public int CategoryId { get; set; }
     public Guid OrganizationId { get; set; }
 }
