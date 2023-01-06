bcb51aef248731c85bdecb6ea3562799dabed37d
Leverow <info.enjoymedia@mail.ru>
2023-01-07 04:51:51 +0500

    Added Request helper to shared project

---
+++ FurnitureShop/front/FurnitureShop.Common/Helpers/IRequestHelper.cs
@@ -0,0 +1,5 @@
+﻿namespace FurnitureShop.Common.Helpers;
+public interface IRequestHelper<in TModel> where TModel : class
+{
+    Task<HttpResponseMessage> SendRequestAsync(HttpMethod method, string baseUri, string? requestUri, TModel? model);
+}
---
+++ FurnitureShop/front/FurnitureShop.Common/Helpers/RequestHelper.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+
+namespace FurnitureShop.Common.Helpers;
+public class RequestHelper<TModel> : IRequestHelper<TModel> where TModel : class
+{
+    private readonly HttpClient _httpClient;
+
+    public RequestHelper(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    public async Task<HttpResponseMessage> SendRequestAsync(HttpMethod method, string baseUri, string? requestUri, TModel? model)
+    {
+        if (method is null)
+            throw new ArgumentNullException(nameof(method));
+        if (baseUri is null)
+            throw new ArgumentNullException(nameof(baseUri));
+
+        if (method == HttpMethod.Post)
+            return await PostRequestAsync(baseUri, requestUri, model);
+        
+        return await GetRequestAsync(baseUri, requestUri);
+    }
+
+    private async Task<HttpResponseMessage> GetRequestAsync(string baseUri, string? requestUri)
+    {
+        _httpClient.BaseAddress = new Uri(baseUri);
+        var requestMessage = new HttpRequestMessage(HttpMethod.Get, requestUri);
+        requestMessage.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+
+        return await _httpClient.SendAsync(requestMessage);
+    }
+
+    private async Task<HttpResponseMessage> PostRequestAsync(string baseUri, string? requestUri, TModel? model)
+    {
+        _httpClient.BaseAddress = new Uri(baseUri);
+        var requestMessage = new HttpRequestMessage(HttpMethod.Post, requestUri);
+        
+        requestMessage.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+
+        if (model is not null)
+            requestMessage.Content = JsonContent.Create(model);
+
+        return await _httpClient.SendAsync(requestMessage);
+    }
+}
