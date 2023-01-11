b248b60c3c84851c294394f6aa076348a6e701bf
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2023-01-11 19:08:19 +0500

    Completed validation for CreateOrganizationDto and UpdateOrganizationDto

--- FurnitureShop/src/FurnitureShop.Merchant.Api/Validators/UpdateOrganizationDtoValidator.cs
+++ FurnitureShop/src/FurnitureShop.Merchant.Api/Validators/UpdateOrganizationDtoValidator.cs
@@ -5,5 +5,6 @@ namespace FurnitureShop.Merchant.Api.Validators;
 public class UpdateOrganizationDtoValidator : AbstractValidator<UpdateOrganizationDto>
 {
     public UpdateOrganizationDtoValidator() =>
-       RuleFor(organizationDto => organizationDto.Name).NotEmpty();
+       RuleFor(organizationDto => organizationDto.Name).Length(1, 30).NotNull().NotEmpty().Matches("^[a-zA-Z']*$")
+           .WithMessage("Organization name must contain only alphabetical characters.");
 }
