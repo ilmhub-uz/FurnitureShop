a3e7c75b72d6345d8c7d74035c8f2f30ce53bc8f
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2023-01-12 04:30:00 +0500

    BaseAddress changed

--- FurnitureShop/front/FurnitureShop.Blazor/Program.cs
+++ FurnitureShop/front/FurnitureShop.Blazor/Program.cs
@@ -8,7 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:1001") });
 builder.Services.AddScoped<CategoryService>();
 builder.Services.AddMudServices();
 
