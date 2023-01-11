78592d6b9f394296c2318930d0a1f2853caa4a78
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2023-01-11 18:50:25 +0500

    Validations for RegisterUserDto have been completed

--- FurnitureShop/src/FurnitureShop.Merchant.Api/Validators/RegisterDtoValidator.cs
+++ FurnitureShop/src/FurnitureShop.Merchant.Api/Validators/RegisterDtoValidator.cs
@@ -7,9 +7,16 @@ public class RegisterDtoValidator : AbstractValidator<RegisterUserDto>
 {
     public RegisterDtoValidator()
     {
-        RuleFor(registerDto => registerDto.UserName).NotNull().Length(5, 40).NotEmpty();
-        RuleFor(registerDto => registerDto.FirstName).NotNull().Length(5, 20).NotEmpty();
-        RuleFor(registerDto => registerDto.LastName).MaximumLength(40).MinimumLength(5);
-        RuleFor(registerDto => registerDto.Password).NotNull().MaximumLength(20).MinimumLength(5);
+        RuleFor(registerDto => registerDto.UserName).NotNull().Length(5, 40).NotEmpty().Must(x =>
+                (Char.IsDigit(x[0]) || x[0] == '_') == false)
+            .WithMessage("'{ PropertyName}' must not start with numeric or special characters")
+            .Matches(@"^[a-zA-Z0-9_]*$")
+            .WithMessage("'{ PropertyName}' must contain one or more special characters.");
+        RuleFor(registerDto => registerDto.FirstName).NotNull().Length(3, 20).NotEmpty().Matches("^[a-zA-Z]*$")
+            .WithMessage("FirstName must contain only alphabetical characters.");
+        RuleFor(registerDto => registerDto.LastName).NotNull().Length(3, 20).NotEmpty().Matches("^[a-zA-Z]*$")
+            .WithMessage("LastName must contain only alphabetical characters.");
+        RuleFor(registerDto => registerDto.Password).NotNull().MaximumLength(40).MinimumLength(5).WithMessage("Password length must be between 5 and 40");
+        RuleFor(registerDto => registerDto.Email).EmailAddress();
     }
 }
