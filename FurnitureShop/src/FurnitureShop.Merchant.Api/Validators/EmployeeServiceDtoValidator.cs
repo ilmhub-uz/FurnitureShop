e49438a1721f7720f22dbe4bab5601dd30278e89
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2023-01-12 02:12:00 +0500

    Completed EmployeeServiceDtoValidator

---
+++ FurnitureShop/src/FurnitureShop.Merchant.Api/Validators/EmployeeServiceDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using FurnitureShop.Merchant.Api.Dtos;
+
+namespace FurnitureShop.Merchant.Api.Validators
+{
+    public class EmployeeServiceDtoValidator : AbstractValidator<EmployeeServiceDto>
+    {
+        public EmployeeServiceDtoValidator()
+        {
+            RuleFor(dto => dto.OrganizationId).NotEmpty().NotNull();
+
+            RuleFor(dto => dto.ERole).NotNull().IsInEnum();
+
+            RuleFor(dto => dto.Email).NotEmpty().NotNull().EmailAddress().Length(5, 100).WithMessage("This is not valid email");
+        }
+    }
+}
