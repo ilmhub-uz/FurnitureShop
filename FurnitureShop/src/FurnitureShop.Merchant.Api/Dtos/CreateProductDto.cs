6b6d856c96ac3f89c471178236838916a70fccd0
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2023-01-12 01:54:39 +0500

    Completed CreateProductValidator and did some fixes

--- FurnitureShop/src/FurnitureShop.Merchant.Api/Dtos/CreateProductDto.cs
+++ FurnitureShop/src/FurnitureShop.Merchant.Api/Dtos/CreateProductDto.cs
@@ -6,10 +6,7 @@ public class CreateProductDto
 {
     public string? Name { get; set; }
     public string? Description { get; set; }
-    ///public Dictionary<string, string>? Properties { get; set; }
     public decimal Price { get; set; }
-    public bool OnTrend { get; set; }
-    public bool OnSale { get; set; }
     public uint Count { get; set; }
     public int CategoryId { get; set; }
     public Guid OrganizationId { get; set; }
