ffb7a089d21310098b90793c7265a6995a019a42
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2023-01-11 19:06:27 +0500

    Renamed SignInDtoValidator

---
+++ FurnitureShop/src/FurnitureShop.Merchant.Api/Validators/SignInDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using FurnitureShop.Merchant.Api.Dtos;
+
+namespace FurnitureShop.Merchant.Api.Validators;
+
+public class SignInDtoValidator : AbstractValidator<LoginUserDto>
+{
+    public SignInDtoValidator()
+    {
+        RuleFor(loginDto => loginDto.UserName).NotNull().Length(5, 40).NotEmpty();
+        RuleFor(loginDto => loginDto.Password).NotNull().Length(5, 20).NotEmpty();
+    }
+}
