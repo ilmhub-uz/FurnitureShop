ff4298be123035e46f940a236a8f9e2173d94377
NiyatovAziz <niyatovazizbek737.com>
2023-01-16 17:48:25 +0500

    MerchantFront/OrganizationServiceAdded

--- FurnitureShop/front/FurnitureShop.Merchant.Blazor/Program.cs
+++ FurnitureShop/front/FurnitureShop.Merchant.Blazor/Program.cs
@@ -2,12 +2,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FurnitureShop.Merchant.Blazor;
 using MudBlazor.Services;
+using FurnitureShop.Merchant.Blazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });   // "https://localhost:1009"
+builder.Services.AddScoped<OrganizationService>();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
---
+++ FurnitureShop/front/FurnitureShop.Merchant.Blazor/Services/OrganizationService.cs
@@ -0,0 +1,85 @@
+﻿using FurnitureShop.Merchant.Blazor.Dtos;
+using FurnitureShop.Merchant.Blazor.ViewModel;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+using System.Reflection;
+
+namespace FurnitureShop.Merchant.Blazor.Services;
+
+public class OrganizationService : HttpClientBase
+{
+    public OrganizationService(HttpClient httpClient) : base(httpClient)
+    {
+
+    }
+
+    public async Task<List<OrganizationView>?> GetOrganizationsAsync()
+    {
+        var httpRequest = new HttpRequestMessage(HttpMethod.Get, "/api/Organizations");
+
+        httpRequest.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+
+        var response = await httpClient.SendAsync(httpRequest);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var organizationsJson = await response.Content.ReadAsStringAsync();
+            var organizations = JsonConvert.DeserializeObject<List<OrganizationView>>(organizationsJson);
+
+            return organizations;
+        }
+
+        return null;
+    }
+    public async Task<OrganizationView?> GetOrganizationByIdAsync(Guid id)
+    {
+        var httpRequest = new HttpRequestMessage(HttpMethod.Get, $"/api/Organizations/{id}");
+
+        httpRequest.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+
+        var response = await httpClient.SendAsync(httpRequest);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var organziationJson = await response.Content.ReadAsStringAsync();
+            var product = JsonConvert.DeserializeObject<OrganizationView>(organziationJson);
+
+            return product;
+        }
+
+        return null;
+    }
+    public async Task<bool> CreateOrganizationAsync(CreateOrganizationDto? createOrganizationDto)
+    {
+        var httpRequest = new HttpRequestMessage(HttpMethod.Post, $"/api/Organizations");
+        httpRequest.Content = JsonContent.Create(createOrganizationDto);
+
+        httpRequest.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+
+        var response = await httpClient.SendAsync(httpRequest);
+
+        if (response.IsSuccessStatusCode)
+        {
+            return true;
+        }
+
+        return false;
+    }
+    public async Task<bool> DeleteOrganizationAsync(Guid id)
+    {
+        var httpRequest = new HttpRequestMessage(HttpMethod.Delete, $"/api/Organizations/{id}");
+
+        httpRequest.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+
+        var response = await httpClient.SendAsync(httpRequest);
+
+        if (response.IsSuccessStatusCode)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+}
