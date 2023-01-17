2a638aa8ffdbd2d0953a3adcb894ffd3845797a1
AbdulazizCoding <abdulazizabduqaxxarov04@gmail.com>
2023-01-17 18:36:39 +0500

    Add organization.razor and all viewModels in Merchant blazor

---
+++ FurnitureShop/front/FurnitureShop.Merchant.Blazor/ViewModel/ContractView.cs
@@ -0,0 +1,9 @@
+﻿namespace FurnitureShop.Merchant.Blazor.ViewModel;
+public class ContractView
+{
+    public string OrderId { get; set; }
+    public string UserId { get; set; }
+    public uint ProductCount { get; set; }
+    public decimal TotalPrice { get; set; }
+    public Dictionary<string, string>? ProductProperties { get; set; }
+}
