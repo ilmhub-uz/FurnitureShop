24dbbb3bec2ec729fc05cd69a012c72f67ccbf75
Leverow <info.enjoymedia@mail.ru>
2023-01-17 21:08:11 +0500

    Dto validators added

---
+++ FurnitureShop/src/FurnitureShop.Admin.Api/Validators/UpdateRoleDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using FurnitureShop.Admin.Api.Dtos;
+
+namespace FurnitureShop.Admin.Api.Validators;
+
+public class UpdateRoleDtoValidator : AbstractValidator<UpdateRoleDto>
+{
+	public UpdateRoleDtoValidator()
+	{
+        RuleFor(roleDto => roleDto.Name).NotNull();
+        RuleFor(roleDto => roleDto.Name).Length(3, 30).When(roleDto => roleDto.Name != null);
+    }
+}
