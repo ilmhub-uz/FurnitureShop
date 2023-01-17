463eb530b4cf34557b789c8670f1711ce35e1b35
Jamoliddin <shermatovjamoliddin3@gmail.com>
2023-01-17 17:13:20 +0500

    added-validator

--- FurnitureShop/src/FurnitureShop.Client.Api/Controllers/AccountController.cs
+++ FurnitureShop/src/FurnitureShop.Client.Api/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using FurnitureShop.Client.Api.Dtos;
+﻿using FluentValidation;
+using FurnitureShop.Client.Api.Dtos;
 using FurnitureShop.Client.Api.Services.Interfaces;
 using FurnitureShop.Common.Filters;
 using FurnitureShop.Data.Context;
@@ -6,6 +7,7 @@ using FurnitureShop.Data.Entities;
 using Mapster;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.IIS.Core;
 
 namespace FurnitureShop.Client.Api.Controllers;
 
@@ -18,22 +20,33 @@ public class AccountController : ControllerBase
     private readonly UserManager<AppUser> _userManager;
     private readonly SignInManager<AppUser> _signInManager;
     private readonly IFileHelperService _fileHelperService;
-
+    private readonly IValidator<RegisterUserDto> _registeruservalidator;
+    private readonly IValidator<LoginUserDto> _loginuservalidator;
     public AccountController(
+        IValidator<RegisterUserDto> registeruservalidator,
         AppDbContext context,
         UserManager<AppUser> userManager,
         SignInManager<AppUser> signInManager,
-        IFileHelperService fileHelperService)
+        IFileHelperService fileHelperService,
+        IValidator<LoginUserDto> loginvalidator)
     {
         _context = context;
         _userManager = userManager;
         _signInManager = signInManager;
+        _loginuservalidator= loginvalidator;
         _fileHelperService = fileHelperService;
+        _registeruservalidator = registeruservalidator;
     }
 
     [HttpPost("sign-up")]
     public async Task<IActionResult> SignUp([FromBody] RegisterUserDto dtoModel)
     {
+        var resultvalidator = _registeruservalidator.Validate(dtoModel);
+        if (!resultvalidator.IsValid)
+        {
+            throw new BadHttpRequestException(resultvalidator.Errors.First().ErrorMessage);
+        }
+
         if (_userManager.Users.Any(u => u.UserName == dtoModel.UserName))
         {
             return BadRequest("User with the username already exists");
@@ -53,6 +66,11 @@ public class AccountController : ControllerBase
     [HttpPost("sign-in")]
     public async Task<IActionResult> SignIn(LoginUserDto dtoModel)
     {
+        var resultvalidator = _loginuservalidator.Validate(dtoModel);
+        if (!resultvalidator.IsValid)
+        {
+            throw new BadHttpRequestException(resultvalidator.Errors.First().ErrorMessage);
+        }
         var result = await _signInManager.PasswordSignInAsync(dtoModel.UserName, dtoModel.Password, true, true);
         if (!result.Succeeded)
             return BadRequest();
