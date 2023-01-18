7552f9326cca8f68a710957fbe4f884183dc838d
NiyatovAziz <niyatovazizbek737.com>
2023-01-18 10:11:38 +0500

    MerchantFront/Service_added

---
+++ FurnitureShop/front/FurnitureShop.Merchant.Blazor/Result.cs
@@ -0,0 +1,18 @@
+﻿namespace FurnitureShop.Merchant.Blazor;
+
+public class Result
+{
+    public string? ErrorMessage { get; set; }
+    public bool IsSuccess { get; set; }
+    public Result(bool isSuccess)
+    {
+        IsSuccess = isSuccess;
+    }
+}
+
+
+public class Result<T> : Result
+{
+    public T? Data { get; set; }
+    public Result(bool isSuccess) : base(isSuccess) { }
+}
