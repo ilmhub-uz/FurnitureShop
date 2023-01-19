1ddbcec3995ed5c2026b5af3cbef9a0ac0345279
NiyatovAziz <niyatovazizbek737.com>
2023-01-19 13:48:54 +0500

    MerchantFront/ProductService_added

--- FurnitureShop/front/FurnitureShop.Merchant.Blazor/Program.cs
+++ FurnitureShop/front/FurnitureShop.Merchant.Blazor/Program.cs
@@ -6,6 +6,7 @@ using FurnitureShop.Merchant.Blazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddScoped<OrganizationService>();
+builder.Services.AddScoped<ProductService>();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
